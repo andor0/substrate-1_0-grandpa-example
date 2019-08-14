f41dd3269ae23104a3a6e0c777d7b33ce669a83e
Luke P <support@deepinthought.io>
2019-08-14 10:53:30 -0500

    fixes grandpa question in Riot chat

---
+++ nodes.sh
@@ -0,0 +1,6 @@
+cargo run --release -- \
+  --base-path /tmp/substrate-1_0-grandpa-example/alice \
+  --chain=local \
+  --alice \
+  --node-key 0000000000000000000000000000000000000000000000000000000000000001 \
+  --validator
