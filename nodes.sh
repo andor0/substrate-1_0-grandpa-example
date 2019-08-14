143f9b233f8d633e25f1807fabc6b778ea54f3eb
Luke P <support@deepinthought.io>
2019-08-14 11:02:23 -0500

    Revert "fixes grandpa question in Riot chat"
    This reverts commit f41dd3269ae23104a3a6e0c777d7b33ce669a83e.

--- nodes.sh
+++
@@ -1,6 +0,0 @@
-cargo run --release -- \
-  --base-path /tmp/substrate-1_0-grandpa-example/alice \
-  --chain=local \
-  --alice \
-  --node-key 0000000000000000000000000000000000000000000000000000000000000001 \
-  --validator
